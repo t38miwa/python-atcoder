82e72a7814e86f336f1255b78f17125e48a8afb3
t38miwa <t38miwa@gmail.com>
2025-10-27 15:13:49 +0900

    feat: 280 b

--- 280/B.py
+++ 280/B.py
@@ -1,11 +1,8 @@
-N=int(input())
+N = int(input())
+S = list(map(int,input().split()))
 
-S=list(map(int,input().split()))
-
-result=[]
-result.append(str(S[0]))
-for i in range(N-1):
-    sum=S[i+1]-S[i]
-    result.append(str(sum))
-
-print(' '.join(result))
+A = [0] * (N)
+A[0] = S[0]
+for i in range(1,N):
+    A[i] = S[i] - S[i-1]
+print(*A)
